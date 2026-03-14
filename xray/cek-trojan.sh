134dca3b621bdf46c36a948361429ab2765e51ce
znandev <136213199+znandev@users.noreply.github.com>
2026-03-15 00:11:16 +0700

    Update cek-trojan.sh

--- xray/cek-trojan.sh
+++ xray/cek-trojan.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
-# Cek login Trojan - by znand-dev
 
-echo -e "🔍 Log koneksi aktif Trojan (Xray access log)"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-log_file="/var/log/xray/access.log"
+clear
 
-if [[ ! -f $log_file ]]; then
-    echo "Log file tidak ditemukan!"
-    exit 1
-fi
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-grep 'accepted' $log_file | awk '{print $3}' | cut -d':' -f1 | sort | uniq -c | sort -nr
+LOG="/var/log/xray/access.log"
+CONFIG="/etc/xray/config.json"
+
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[44;1;39m            CEK LOGIN TROJAN USER            \E[0m"
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo ""
+
+printf "%-15s %-18s\n" "USERNAME" "IP CLIENT"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
+users=$(jq -r '.inbounds[] | select(.tag=="trojan-tls") | .settings.clients[].email' $CONFIG)
+
+ips=$(grep "accepted" $LOG \
+| awk -F'from ' '{print $2}' \
+| cut -d':' -f1 \
+| grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' \
+| grep -v "127.0.0.1" \
+| sort -u)
+
+for user in $users
+do
+for ip in $ips
+do
+printf "${GREEN}%-15s${NC} %-18s\n" "$user" "$ip"
+done
+done
+
+echo ""
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+
+read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
+
+m-trojan
