15417c41fcf929fa77c90bc0fb0162a8b51b37a1
znandev <136213199+znandev@users.noreply.github.com>
2026-03-15 00:07:20 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -1,57 +1,45 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com)
-echo "Checking VPS"
+
 clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m        Add VLESS Account        \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
-# Load domain
-source /var/lib/ipvps.conf
-if [[ "$IP" = "" ]]; then
-  domain=$(cat /etc/xray/domain)
-else
-  domain=$IP
-fi
+# ambil domain
+domain=$(cat /etc/xray/domain)
 
-# Ambil port TLS & non-TLS dari log-install
-tls="$(grep -w "Vless WS TLS" ~/log-install.txt | cut -d: -f2 | tr -d ' ')"
-none="$(grep -w "Vless WS none TLS" ~/log-install.txt | cut -d: -f2 | tr -d ' ')"
+# ambil port dari log install
+tls=$(grep -w "XRAY TLS" /root/log-install.txt | cut -d: -f2 | tr -d ' ')
+none=$(grep -w "XRAY None TLS" /root/log-install.txt | cut -d: -f2 | tr -d ' ')
 
-# Input user
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  echo -e "\E[44;1;39m       Add Vless Account        \E[0m"
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  read -rp "Username: " -e user
-  CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+read -rp "Username : " user
+read -rp "Expired (days): " masaaktif
 
-  if [[ ${CLIENT_EXISTS} == '1' ]]; then
-    echo -e "\n\033[1;31mUser '$user' sudah ada, coba nama lain!\033[0m"
-    read -n 1 -s -r -p "Tekan apa saja untuk kembali..."
-    m-vless
-    exit
-  fi
-done
-
-# Generate UUID & tanggal expired
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (hari): " masaaktif
 exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 
-# Tambah user ke config
-sed -i '/#vless$/a\#& '"$user $exp"'\n},{"id": "'"$uuid"'","email": "'"$user"'"' /etc/xray/config.json
-sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\n},{"id": "'"$uuid"'","email": "'"$user"'"' /etc/xray/config.json
+# inject user ke config
+jq --arg uuid "$uuid" --arg user "$user" '
+(.inbounds[] | select(.tag=="vless-tls").settings.clients) +=
+[{"id":$uuid,"email":$user}] |
+(.inbounds[] | select(.tag=="vless-nontls").settings.clients) +=
+[{"id":$uuid,"email":$user}]
+' /etc/xray/config.json > /tmp/config.json
 
-# Generate link
+mv /tmp/config.json /etc/xray/config.json
+
+# generate vless link
 vlesslink1="vless://${uuid}@${domain}:${tls}?path=/vless&security=tls&encryption=none&type=ws#${user}"
+
 vlesslink2="vless://${uuid}@${domain}:${none}?path=/vless&encryption=none&type=ws#${user}"
+
 vlesslink3="vless://${uuid}@${domain}:${tls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 
-# Restart xray
 systemctl restart xray
 
-# Output
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
-echo -e "\E[44;1;39m        XRAY Vless ACCOUNT        \E[0m" | tee -a /etc/log-create-vless.log
+echo -e "\E[44;1;39m        XRAY VLESS ACCOUNT        \E[0m" | tee -a /etc/log-create-vless.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Remarks        : ${user}" | tee -a /etc/log-create-vless.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-vless.log
@@ -66,12 +54,14 @@ echo -e "Path           : /vless" | tee -a /etc/log-create-vless.log
 echo -e "ServiceName    : vless-grpc" | tee -a /etc/log-create-vless.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Link gRPC      : ${vlesslink3}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Expired On     : ${exp}" | tee -a /etc/log-create-vless.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
-echo "" | tee -a /etc/log-create-vless.log
+echo ""
+
 read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
-
 m-vless
-
