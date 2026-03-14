52357f4dd6a186cbfe6be2d23c380104c6c59ab8
znandev <136213199+znandev@users.noreply.github.com>
2026-03-15 00:10:11 +0700

    Update add-trojan.sh

--- xray/add-trojan.sh
+++ xray/add-trojan.sh
@@ -1,73 +1,58 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com)
-echo "Checking VPS"
+
 clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m        Add TROJAN Account       \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
-# Load domain
-source /var/lib/ipvps.conf
-if [[ "$IP" = "" ]]; then
-  domain=$(cat /etc/xray/domain)
-else
-  domain=$IP
-fi
+# ambil domain
+domain=$(cat /etc/xray/domain)
 
-# Ambil port dari log-install
-tls="$(grep -w "Trojan WS TLS" ~/log-install.txt | cut -d: -f2 | tr -d ' ')"
-none="$(grep -w "Trojan WS none TLS" ~/log-install.txt | cut -d: -f2 | tr -d ' ')"
+# ambil port TLS
+tls=$(grep -w "XRAY TLS" /root/log-install.txt | cut -d: -f2 | tr -d ' ')
 
-# Input user
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  echo -e "\E[44;1;39m      Add Trojan Account       \E[0m"
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  read -rp "Username: " -e user
-  CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+read -rp "Username : " user
+read -rp "Expired (days): " masaaktif
 
-  if [[ ${CLIENT_EXISTS} == '1' ]]; then
-    echo -e "\n\033[1;31mUser '$user' sudah ada, coba nama lain!\033[0m"
-    read -n 1 -s -r -p "Tekan apa saja untuk kembali..."
-    m-trojan
-    exit
-  fi
-done
-
-# Generate password & expired
-trojan_pwd=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (hari): " masaaktif
+uuid=$(cat /proc/sys/kernel/random/uuid)
 exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 
-# Tambah user ke config
-sed -i '/#trojan$/a\#& '"$user $exp"'\n},{"password": "'"$trojan_pwd"'","email": "'"$user"'"' /etc/xray/config.json
+CONFIG="/etc/xray/config.json"
 
-# Generate link
-trojanlink1="trojan://${trojan_pwd}@${domain}:${tls}?path=/trojan&security=tls&type=ws#${user}"
-trojanlink2="trojan://${trojan_pwd}@${domain}:${none}?path=/trojan&type=ws#${user}"
-trojanlink3="trojan://${trojan_pwd}@${domain}:${tls}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
+# inject user ke config
+jq --arg uuid "$uuid" --arg user "$user" '
+(.inbounds[] | select(.tag=="trojan-tls").settings.clients) +=
+[{"password":$uuid,"email":$user}]
+' $CONFIG > /tmp/config.json
 
-# Restart xray
+mv /tmp/config.json $CONFIG
+
+# generate trojan link
+trojanlink1="trojan://${uuid}@${domain}:${tls}?path=/trojan&security=tls&type=ws#${user}"
+
+trojanlink2="trojan://${uuid}@${domain}:${tls}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
+
 systemctl restart xray
 
-# Output
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
-echo -e "\E[44;1;39m       XRAY Trojan ACCOUNT       \E[0m" | tee -a /etc/log-create-trojan.log
+echo -e "\E[44;1;39m        XRAY Trojan ACCOUNT       \E[0m" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo -e "Remarks        : ${user}" | tee -a /etc/log-create-trojan.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-trojan.log
 echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-trojan.log
-echo -e "Port none TLS  : ${none}" | tee -a /etc/log-create-trojan.log
-echo -e "Port gRPC      : ${tls}" | tee -a /etc/log-create-trojan.log
-echo -e "Password       : ${trojan_pwd}" | tee -a /etc/log-create-trojan.log
+echo -e "Password       : ${uuid}" | tee -a /etc/log-create-trojan.log
 echo -e "Network        : ws / grpc" | tee -a /etc/log-create-trojan.log
 echo -e "Path           : /trojan" | tee -a /etc/log-create-trojan.log
 echo -e "ServiceName    : trojan-grpc" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo -e "Link TLS       : ${trojanlink1}" | tee -a /etc/log-create-trojan.log
-echo -e "Link none TLS  : ${trojanlink2}" | tee -a /etc/log-create-trojan.log
-echo -e "Link gRPC      : ${trojanlink3}" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Link gRPC      : ${trojanlink2}" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo -e "Expired On     : ${exp}" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
-echo "" | tee -a /etc/log-create-trojan.log
-read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
+echo ""
 
+read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
 m-trojan
