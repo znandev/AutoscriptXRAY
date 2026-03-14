c95b46781984a753f1fc49cc7af1d822a5f0fb8c
znandev <136213199+znandev@users.noreply.github.com>
2026-03-14 15:48:46 +0700

    Update add-vmess.sh

--- xray/add-vmess.sh
+++ xray/add-vmess.sh
@@ -1,80 +1,53 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com)
-echo "Checking VPS"
+
 clear
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "       Add VMess User"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
-# Load domain dari config
-source /var/lib/ipvps.conf
-if [[ "$IP" = "" ]]; then
-  domain=$(cat /etc/xray/domain)
-else
-  domain=$IP
-fi
-
-# Ambil port dari log
-tls=$(grep -w "XRAY TLS" /root/log-install.txt | cut -d: -f2 | tr -d ' ')
-none=$(grep -w "XRAY None TLS" /root/log-install.txt | cut -d: -f2 | tr -d ' ')
-
-# Input username
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  echo -e "\E[44;1;39m       Add VMess Account        \E[0m"
-  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  read -rp "Username: " -e user
-  CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
-
-  if [[ ${CLIENT_EXISTS} == '1' ]]; then
-    echo ""
-    echo -e "\033[1;31mUser '$user' sudah ada, coba nama lain!\033[0m"
-    read -n 1 -s -r -p "Tekan apa saja untuk kembali..."
-    m-vmess
-    exit
-  fi
-done
+read -rp "Username : " user
+read -rp "Expired (days): " masaaktif
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (hari): " masaaktif
 exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 
-# Inject user ke config
-sed -i "/#vmess-tls\$/a\#& $user $exp\n      ,{\"id\": \"$uuid\",\"alterId\": 0,\"email\": \"$user\"}" /etc/xray/config.json
-sed -i "/#vmess-nontls\$/a\#& $user $exp\n      ,{\"id\": \"$uuid\",\"alterId\": 0,\"email\": \"$user\"}" /etc/xray/config.json
+domain=$(cat /etc/xray/domain)
 
-# Buat link
-vmesslink1="vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"${user}\",\"add\":\"${domain}\",\"port\":\"${tls}\",\"id\":\"${uuid}\",\"aid\":\"0\",\"net\":\"ws\",\"path\":\"/vmess\",\"type\":\"none\",\"host\":\"${domain}\",\"tls\":\"tls\"}" | base64 -w 0)"
-vmesslink2="vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"${user}\",\"add\":\"${domain}\",\"port\":\"${none}\",\"id\":\"${uuid}\",\"aid\":\"0\",\"net\":\"ws\",\"path\":\"/vmess\",\"type\":\"none\",\"host\":\"${domain}\",\"tls\":\"none\"}" | base64 -w 0)"
-vmesslink3="vmess://${uuid}@${domain}:${tls}?mode=gun&security=tls&type=grpc&serviceName=vmess-grpc&sni=bug.com#${user}"
+CONFIG="/etc/xray/config.json"
 
-# Restart Xray
+# tambah user ke vmess TLS
+jq --arg uuid "$uuid" --arg user "$user" '
+(.inbounds[] | select(.tag=="vmess-tls").settings.clients) += 
+[{"id":$uuid,"alterId":0,"email":$user}]
+' $CONFIG > /tmp/config.json
+
+mv /tmp/config.json $CONFIG
+
+# tambah user ke vmess non TLS
+jq --arg uuid "$uuid" --arg user "$user" '
+(.inbounds[] | select(.tag=="vmess-nontls").settings.clients) += 
+[{"id":$uuid,"alterId":0,"email":$user}]
+' $CONFIG > /tmp/config.json
+
+mv /tmp/config.json $CONFIG
+
 systemctl restart xray
 
-# Output
-clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "\E[44;1;39m        XRAY VMess ACCOUNT        \E[0m" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "Remarks        : ${user}" | tee -a /etc/log-create-vmess.log
-echo -e "Domain         : ${domain}" | tee -a /etc/log-create-vmess.log
-echo -e "Wildcard       : (bug.com).${domain}" | tee -a /etc/log-create-vmess.log
-echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-vmess.log
-echo -e "Port none TLS  : ${none}" | tee -a /etc/log-create-vmess.log
-echo -e "Port gRPC      : ${tls}" | tee -a /etc/log-create-vmess.log
-echo -e "id             : ${uuid}" | tee -a /etc/log-create-vmess.log
-echo -e "Alter ID       : 0" | tee -a /etc/log-create-vmess.log
-echo -e "Encryption     : auto" | tee -a /etc/log-create-vmess.log
-echo -e "Network        : ws / grpc" | tee -a /etc/log-create-vmess.log
-echo -e "Path           : /vmess" | tee -a /etc/log-create-vmess.log
-echo -e "ServiceName    : vmess-grpc" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "Link TLS       : ${vmesslink1}" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "Link none TLS  : ${vmesslink2}" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "Link gRPC      : ${vmesslink3}" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "Expired On     : ${exp}" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo "" | tee -a /etc/log-create-vmess.log
-read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
+echo ""
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " XRAY VMESS ACCOUNT"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
+echo "User   : $user"
+echo "Domain : $domain"
+echo "UUID   : $uuid"
+echo "Exp    : $exp"
+
+vmesslink="vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"${user}\",\"add\":\"${domain}\",\"port\":\"443\",\"id\":\"${uuid}\",\"aid\":\"0\",\"net\":\"ws\",\"path\":\"/vmess\",\"type\":\"none\",\"host\":\"${domain}\",\"tls\":\"tls\"}" | base64 -w 0)"
+
+echo ""
+echo "VMESS LINK"
+echo "$vmesslink"
+echo ""
 
 m-vmess
